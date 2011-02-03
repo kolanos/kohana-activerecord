f510e0684e609d622d27ea60b43911a83206d600
Devi Mandiri <devi.mandiri@gmail.com>
2011-02-03 23:02:00 +0700

    initial commit

--- init.php
+++ init.php
@@ -2,30 +2,9 @@
 
 include Kohana::find_file('vendor', 'activerecord/ActiveRecord');
 
-/*$name = 'default';
-	
-$db_config = Kohana::config('database')->$name;
-		
-extract($db_config['connection'] + array(
-	'database'   => '',
-	'hostname'   => '',
-	'username'   => '',
-	'password'   => '',
-	'persistent' => FALSE,
-));
-		
-unset($db_config['connection']['username'], $db_config['connection']['password']);
-		        
-$dsn = $db_config['type'].'://'.$username.':'.$password.'@'.$hostname.'/'.$database;
-       
-ActiveRecord\Config::instance()
-	->set_model_directory(APPPATH.'classes/model')
-	->set_connections(array('development' => $dsn ));
-*/	
-
 ActiveRecord\Config::initialize(function($cfg)
 {
-	$db = Kohana::config('mesin');
+	$db = Kohana::config('activerecord');
 	$cfg->set_model_directory($db->model);
 	$cfg->set_connections(array('development' => $db->dsn ));	
 });
